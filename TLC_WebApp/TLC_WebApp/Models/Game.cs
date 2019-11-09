81d532382ea1f8e4c887664621cc91bce3f7c65d
BenHoople <BenHoople@users.noreply.github.com>
2019-11-08 19:37:38 -0500

    renaming file
    there were problems with the file being TLC-Assignment
    so i renamed it
    TLC_Assignment

---
+++ TLC_WebApp/TLC_WebApp/Models/Game.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TLC_WebApp.Models
+{
+    public class Game
+    {
+        private char[] gameBoard = new char[9];
+        private char x = 'X', o = 'O', fill = '-';
+        private byte choice;
+        private char turn;
+        //private Scanner scanner = new Scanner(System.in);
+        private bool playable;
+
+        public Game()
+        {
+            this.gameBoard = fillBoard();
+            this.turn = x;
+            this.playable = true;
+        }
+        public void move()
+        {
+            this.choice = takeInput();
+            this.gameBoard[choice - 1] = this.turn;
+            if (this.turn == x)
+            {
+                this.turn = o;
+            }
+            else
+            {
+                this.turn = x;
+            }
+        }
+        private byte takeInput()
+        {
+
+            //return scanner.nextByte();
+            return 10;
+        }
+
+        private char[] fillBoard()
+        {
+            int i = 0;
+            for (int x = 0; x < this.gameBoard.Length; x++)
+            {
+                this.gameBoard[x] = this.fill;
+                i++;
+                if (i == 3)
+                {
+                    i = 0;
+                }
+            }
+            return gameBoard;
+        }
+
+        public bool isPlayable()
+        {
+            return playable;
+        }
+    }
+}
