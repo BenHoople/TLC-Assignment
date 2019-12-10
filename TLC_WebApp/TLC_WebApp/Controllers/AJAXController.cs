cd672b5a52fc620ae9bc4d8cec24dcc7172f7f42
BenHoople <BenHoople@users.noreply.github.com>
2019-12-10 12:27:02 -0500

    ajax partial loads
    now to fix the location of methods

---
+++ TLC_WebApp/TLC_WebApp/Controllers/AJAXController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TLC_WebApp.Controllers
+{
+    public class AJAXController : Controller
+    {
+        public IActionResult GameBoardPartial()
+        {
+            return PartialView();
+        }
+    }
+}
