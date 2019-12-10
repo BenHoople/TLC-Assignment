dc5bb8a42eb4ada09c94596e1ea7da0717aa19a0
BenHoople <BenHoople@users.noreply.github.com>
2019-12-09 21:49:41 -0500

    i hate binaries

--- TLC_WebApp/TLC_WebApp/Controllers/GameBoardsController.cs
+++ TLC_WebApp/TLC_WebApp/Controllers/GameBoardsController.cs
@@ -14,11 +14,33 @@ namespace TLC_WebApp.Controllers
     {
         private readonly ApplicationDbContext _context;
 
+        private List<GameBoard> gameBoards = new List<GameBoard>();
+
         public GameBoardsController(ApplicationDbContext context)
         {
             _context = context;
+            GetDataBaseData();
         }
+
+        private async void GetDataBaseData()
+        {
+            try
+            {
+                gameBoards = _context.GameBoard.ToList();
+            }
+            catch (Exception e)
+            {
+                RedirectToAction(nameof(Index));
+            }
+        }
+
         public static Game game = new Game();
+
+        public RedirectToActionResult NewGame()
+        {
+            game = new Game();
+            return RedirectToAction(nameof(Index));
+        }
 
         // GET: GameBoards
         public async Task<IActionResult> Index()
@@ -26,59 +48,55 @@ namespace TLC_WebApp.Controllers
             return View(game);
         }
         //verify position of clicked button.
-        public RedirectToActionResult Clicked(String position)
+        public async Task<IActionResult> Clicked(String position)
         {
-            switch (position)
+            //player choice
+            if (game.playable)//make sure game is playable
             {
-                case "TopLeft":
-                    if (game.isMoveValid(game.gb.TopLeft))
-                    {
-                        game.gb.TopLeft = game.turn;
-                        game.move();
-                    }
-                    else
-                    {
-                       // new GCNotificationStatus();
-                    }
-                    break;
-                case "TopMiddle":
-                    game.gb.TopMiddle = game.turn;
-                    game.move();
-                    break;
-                case "TopRight":
-                    game.gb.TopRight = game.turn;
-                    game.move();
-                    break;
-                case "MiddleLeft":
-                    game.gb.MiddleLeft = game.turn;
-                    game.move();
-                    break;
-                case "MiddleMiddle":
-                    game.gb.MiddleMiddle = game.turn;
-                    game.move();
-                    break;
-                case "MiddleRight":
-                    game.gb.MiddleRight = game.turn;
-                    game.move();
-                    break;
-                case "BottomLeft":
-                    game.gb.BottomLeft = game.turn;
-                    game.move();
-                    break;
-                case "BottomMiddle":
-                    game.gb.BottomMiddle = game.turn;
-                    game.move();
-                    break;
-                case "BottomRight":
-                    game.gb.BottomRight = game.turn;
-                    game.move();
-                    break;
-                default:
-                    break;
+                game.decision(position);//make the move
+                
+                if (!gameBoards.Contains(game.gb))//check to see if the gameboard exists in the database
+                {//here i've determined it not to be in the database so i have to add it.
+                    addToDatabase();
+                    GetDataBaseData();//by updating the gameBoards list i'll be able to get the gameboard ID for later updating
+                }
+                    //yes im searching the list twice in a row which seems redundant but not sure how else to do it
+                    //this should change the game.gb board to numbers for ai searching and give it an ID to put into the Turns list
+                    game.gb = gameBoards.Find(p => p.Equals(game.gb));
             }
+            //AI choice
+            if (game.playable)//game should be playable unless there are no turns to make or if x won
+            {
+                game.AIChoice(); //choose the highest number or go for broke
+            }
+            //learn
+            if (!game.playable)
+            {
+                foreach (GameBoard gameBoard in gameBoards)
+                {
+                    foreach (Turn turn in game.turnTracker)
+                    {
+                        if (turn.ID == gameBoard.ID)
+                        {
+                            gameBoard.UpdateAI(turn.position, game.learningRate);
+                            _context.GameBoard.Update(gameBoard);
+                            await _context.SaveChangesAsync();//needed coding stuff
+                        }
+                    }
+                }
+            }
+            game.gb.ResetGameBoard();
             return RedirectToAction(nameof(Index));
         }
-            // GET: GameBoards/Details/5
+
+        private void addToDatabase()
+        {
+            game.gb.SendToDatabase();//here i will make all "-"'s = 0's so the math can begin
+            _context.Add(game.gb);//i'll add the game to the database here
+            _context.SaveChanges();//needed coding stuff
+        }
+
+        // GET: GameBoards/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -95,13 +113,14 @@ namespace TLC_WebApp.Controllers
 
             return View(gameBoard);
         }
-
+        /*
         // GET: GameBoards/Create
         public IActionResult Create()
         {
             return View();
         }
-
+        */
+        /*
         // POST: GameBoards/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -117,7 +136,9 @@ namespace TLC_WebApp.Controllers
             }
             return View(gameBoard);
         }
-
+       */
+        //delete and edit code incase its needed
+        /*
         // GET: GameBoards/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -197,6 +218,8 @@ namespace TLC_WebApp.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        */
+
 
         private bool GameBoardExists(int id)
         {
@@ -205,12 +228,51 @@ namespace TLC_WebApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> onclick()
+        public async void TeachTheAI() 
         {
+            int number = 100;
+            //for x number of times, Play the Game
+            for (int i = 0; i <= number; i++)
+            {
+                for (int x = 0; game.playable && x < 5; x++)
+                {
+                    //make random decision
+                    if (game.playable)
+                    {
+                        game.makeRandomMove();
 
-            GameBoard gb = new GameBoard();
-            gb.TopLeft = "X";
-            return View(); ;
+                        if (!gameBoards.Any(p => p.Equals(game.gb)))
+                        {
+                            //addToDatabase();
+                            game.gb.SendToDatabase();//here i will make all "-"'s = 0's so the math can begin
+                            _context.Add(game.gb);//i'll add the game to the database here
+                            _context.SaveChanges();//needed coding stuff
+                            GetDataBaseData();
+                        }
+                        game.gb = gameBoards.Find(p => p.Equals(game.gb));
+                    }
+                    //AI choice (educated decision)
+                    if (game.playable)
+                    game.AIChoice();
+
+
+                    game.gb.ResetGameBoard();
+                }
+                //learn
+                foreach (GameBoard gameBoard in gameBoards)
+                {
+                    foreach (Turn turn in game.turnTracker)
+                    {
+                        if (turn.ID == gameBoard.ID)
+                        {
+                            gameBoard.UpdateAI(turn.position, game.learningRate);
+                            _context.GameBoard.Update(gameBoard);
+                            _context.SaveChanges();//needed coding stuff
+                        }
+                    }
+                }
+                game = new Game();
+            }//repeat
         }
     }
 }
